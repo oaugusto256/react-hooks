f1d5b413b372106ea563b00c771e0caa85db6c16
Otavio Augusto <tavioalves@gmail.com>
2019-04-19 16:14:22 -0300

    Resource list component uses componentDidUpdate life cycle method

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,7 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class ResourceList extends Component {
+  state = {
+    resource: []
+  };
+
+  componentDidMount = async () => {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/${this.props.resource}`
+    );
+
+    this.setState({ resource: response.data });
+  };
+
+  componentDidUpdate = async prevProps => {
+    if (prevProps.resource !== this.props.resource) {
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/${this.props.resource}`
+      );
+
+      this.setState({ resource: response.data });
+    }
+  };
+
   render() {
-    return <div>{this.props.resource}</div>;
+    return <div>{this.state.resource.length}</div>;
   }
 }

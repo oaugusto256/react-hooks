9e8aba30b4a19fdbac73fb4a99c0594e34d91fce
Otavio Augusto <tavioalves@gmail.com>
2019-04-19 20:14:00 -0300

    Example of code reuse using hooks

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,20 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const ResourceList = ({ resource }) => {
+const useResources = resource => {
   const [resources, setResources] = useState([]);
 
-  const fetchResource = async resource => {
-    const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/${resource}`
-    );
-
-    setResources(response.data);
-  };
-
   useEffect(() => {
-    fetchResource(resource);
+    (async resource => {
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/${resource}`
+      );
+
+      setResources(response.data);
+    })(resource);
   }, [resource]);
+
+  return resources;
+};
+
+const ResourceList = ({ resource }) => {
+  const resources = useResources(resource);
 
   return (
     <ul>

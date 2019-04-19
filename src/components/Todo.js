88055558e98194b667aa40a410494931d07f9fb9
Otavio Augusto <tavioalves@gmail.com>
2019-04-19 15:12:36 -0300

    Add functionality for a simple statefull component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
 
 export default class Todo extends Component {
+  state = {
+    resource: 'posts'
+  };
+
   render() {
-    return <div>Todo</div>;
+    return (
+      <>
+        <h1>React Hooks</h1>
+        <div>
+          <div>
+            <button onClick={() => this.setState({ resource: 'todos ' })}>
+              Todos
+            </button>
+            <button onClick={() => this.setState({ resource: 'posts ' })}>
+              Posts
+            </button>
+          </div>
+          {this.state.resource}
+        </div>
+      </>
+    );
   }
 }
